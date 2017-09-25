4b615fd9707bc39722af7a42f9bf285290f00d0f
Fredson Ricelli <fredseph@gmail.com>
2017-09-24 21:19:00 -0300

    Xunit não detectava testes. Os default da MS funcionou.

---
+++ SWUnitTests/Business/PromotionBusinessTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SWBusiness;
+using SWDomain.DataTransferObjects;
+using SWDomain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWUnitTests.Business
+{
+    [TestClass]
+    public class PromotionBusinessTests
+    {
+        [TestMethod]
+        public void TestApplyPromotion0()
+        {
+            var cartItem = new DTOCartItem() { Item = new Item() { Id = 0, Name = "", Price = 3.0m, Promotion = 0 }, Count = 2 };
+            decimal result = new PromotionBusiness().ApplyPromotion(cartItem);
+
+            object.Equals(result, 3.0m);
+        }
+
+        [TestMethod]
+        public void TestApplyPromotion1()
+        {
+            var cartItem = new DTOCartItem() { Item = new Item() { Id = 0, Name = "", Price = 2.0m, Promotion = 1 }, Count = 2 };
+            decimal result = new PromotionBusiness().ApplyPromotion(cartItem);
+
+            object.Equals(result, 2.0m);
+        }
+
+        [TestMethod]
+        public void TestApplyPromotion2()
+        {
+            var cartItem = new DTOCartItem() { Item = new Item() { Id = 0, Name = "", Price = 4.0m, Promotion = 2 }, Count = 3 };
+            decimal result = new PromotionBusiness().ApplyPromotion(cartItem);
+
+            object.Equals(result, 10.0m);
+        }
+    }
+}
--- SWUnitTests/Properties/AssemblyInfo.cs
+++ SWUnitTests/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("SWUnitTest")]
+[assembly: AssemblyTitle("SWUnitTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SWUnitTest")]
+[assembly: AssemblyProduct("SWUnitTests")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("5a31380c-256a-4ed2-81ae-6e5dc3aee1ee")]
+[assembly: Guid("16d7d038-8742-4266-9013-0b50b5e6b98a")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
